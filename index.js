a057f9bae4b06bb4cde4e6fd179f339017d71d75
lorna-chepkirui <lornachepkirui20@gmail.com>
2022-06-14 07:19:51 +0300

    commit tree bafc513302b9720e582ed9e01ea18511a2067ed6

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+const superbowlWin = (records) => {
+    let wins = records.find((record) => record.result === "W");
+    // => {result: "", year: ""} || undefined
+  
+    //   return undefined if wins is undefined otherwise returns wins.year value
+    return wins == undefined ? undefined : wins.year;
+  }; 
+  
